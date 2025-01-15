053487cb02c837f0926e9a71d47d4b0eb9937f9d
Sanil Sandeep <sanil.sandeep001@gmail.com>
2025-01-15 19:58:51 +0530

    Update

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes)
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
     console.log("Server is running on port : " + PORT);
