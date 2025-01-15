3a2e72664f4e10fa1431762a1b2d0b468688bf02
Sanil Sandeep <sanil.sandeep001@gmail.com>
2025-01-15 19:14:29 +0530

    Create message.controller.js

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -4,6 +4,6 @@ import { protectRoute } from "../middleware/auth.middleware";
 
 const router = express.Router();
 
-router.get("/users",protectRoute, getUserForSidebar)
+router.get("/users",protectRoute, getUsersForSidebar)
 
 export default router;
