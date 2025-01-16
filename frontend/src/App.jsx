428050cf7ef7ec82f3eedd77beafbd53e8d16c35
Sanil Sandeep <sanil.sandeep001@gmail.com>
2025-01-16 18:47:10 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/signup' element={<SignUpPage/>} />
+        <Route path='/login' element={<LoginPage/>} />
+        <Route path='/settings' element={<SettingPage/>} />
+        <Route path='/profile' element={<ProfilePage/>} />
+      </Routes>
     </div>
   )
 }
