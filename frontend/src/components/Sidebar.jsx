6893ddde755b337f7d781df4acf6ebdfe144b140
Sanil Sandeep <sanil.sandeep001@gmail.com>
2025-01-22 18:54:50 +0530

    Update

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -2,11 +2,12 @@ import React, { useEffect } from 'react'
 import { useChatStore } from '../store/useChatStore'
 import SidebarSkeleton from './sceletons/SidebarSkeleton';
 import { Users } from 'lucide-react';
+import { useAuthStore } from '../store/useAuthStore';
 
 const Sidebar = () => {
   const { getUsers, users, selectedUser, setSelectedUser, isUsersLoading } = useChatStore();
 
-  const onlineUsers = [];
+  const {onlineUsers} = useAuthStore();
 
   useEffect(() => {
     getUsers()
