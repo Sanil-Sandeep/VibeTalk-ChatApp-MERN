a7adc48c5c6a793e7e1fc3434704f18abf72f0b0
Sanil Sandeep <sanil.sandeep001@gmail.com>
2025-01-24 17:53:24 +0530

    Update

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const MessageInput = () => {
+    const [text, setText] = useState("");
+    const [imagePreview, setImagePreview] = useState(null);
+    const fileInputRef = useRef(null);
   return (
     <div>MessageInput</div>
   )
